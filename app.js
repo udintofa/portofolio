75f9eb9a5426c73b22e7d1eb5b1a894f27bd6047
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2025-03-18 14:27:48 +0700

    nambah port dan memperbaiki beberap templates

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-
+// Melayani file statis dari folder 'public'
 app.use(express.static('public'));
 
-//route
+// Route utama
 app.get('/', (req, res) => {
   res.sendFile('./pages/index.html', {root: __dirname});
 });
@@ -14,18 +13,21 @@ app.get('/contact', (req, res) => {
 });
 app.get('/profiles', (req, res) => {
   res.sendFile('./pages/profiles.html', {root: __dirname});
-})
+});
 app.get('/skills', (req, res) => {
   res.sendFile('./pages/skills.html', {root: __dirname});
-})
+});
 app.get('/experience', (req, res) => {
   res.sendFile('./pages/experience.html', {root: __dirname});
-})
-app.use('/', (req, res) => {
-    res.send('<h1>404</h1>');
-}) //dijalankan terus, digunakan untuk link yg tidak ada saja
+});
 
+// Middleware 404
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 
+// Menggunakan PORT dari Vercel atau default 3000 saat lokal
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server berjalan pada port ${port}`);
-})
+});
